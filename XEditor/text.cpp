9b9f0b99abb981a93d231a81f16362c3ed183812
tkox <1945483930@qq.com>
2017-10-28 15:29:46 +0800

    add text.cpp

---
+++ XEditor/text.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+ > File Name    : text.cpp
+ > Author       : xuxuegang
+ > Emali        : xuegangxu@icloud.com
+ > Created Time : 2017年10月28日 15时28分56秒 CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
